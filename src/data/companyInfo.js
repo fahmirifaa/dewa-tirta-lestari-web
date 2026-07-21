aed5ec2c4121f54b2794b8828f5febc9cbc4bda4
fahmi1804 <fahmi.86juelek@gmail.com>
2026-07-21 23:00:38 +0700

    update maps

--- src/data/companyInfo.js
+++ src/data/companyInfo.js
@@ -1,21 +1,13 @@
 export const companyInfo = {
   name: "DEWA TIRTA LESTARI",
   tagline: "Supplier Air Bersih Terpercaya untuk Segala Kebutuhan",
-
-  address:
-    "Sarongge, Jl. Raya Serang - Pandeglang Kp No.Km 9, Kemanisan, Kec. Curug, Kota Serang, Banten 42171",
-
+  address: "Sarongge, Jl. Raya Serang - Pandeglang Kp No.Km 9, Kemanisan, Kec. Curug, Kota Serang, Banten 42171",
   phone: "083842931575",
   whatsapp: "6283842931575",
   email: "info@dewatirtalestari.co.id",
-
-  operationalHours: "24 Jam Setiap Hari",
-
+  operationalHours: "Setiap Hari, 24 Jam",
   mapEmbedUrl:
-    "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d471.27676593209577!2d106.137023!3d-6.2013136!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e42212d88f8e291%3A0x17b0955cc7f27a51!5e1!3m2!1sid!2sid!4v1784648528415!5m2!1sid!2sid",
-
-  googleMapsUrl: "https://maps.app.goo.gl/mZGJjJGgBgSh3LNY6",
-
+    "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d471.27676593209577!2d106.137023!3d-6.2013136!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e42212d88f8e291%3A0x17b0955cc7f27a51!2sPengisian%20Tanki%20Air%20Bersih%20(Pump)!5e1!3m2!1sid!2sid!4v1784648528415!5m2!1sid!2sid",
   socialMedia: {
     facebook: "https://facebook.com/dewatirtalestari",
     instagram: "https://instagram.com/dewatirtalestari",
