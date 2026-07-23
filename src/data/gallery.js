2fe28603a1d0ebd6466b07336a4130e436dd97c3
fahmi1804 <fahmi.86juelek@gmail.com>
2026-07-23 16:48:26 +0700

    repairing galerry.js

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -2,54 +2,42 @@ import galeri1 from "../assets/images/galeri/galeri-1.jpg";
 import galeri2 from "../assets/images/galeri/galeri-2.jpg";
 import galeri3 from "../assets/images/galeri/galeri-3.jpg";
 import galeri4 from "../assets/images/galeri/galeri-4.jpg";
+import armada5 from "../assets/images/galeri/galeri-armada-5.png";
+import armada6 from "../assets/images/galeri/galeri-armada-6.png";
+import armada7 from "../assets/images/galeri/galeri-armada-7.png";
+import armada8 from "../assets/images/galeri/galeri-armada-8.png";
+import armada9 from "../assets/images/galeri/galeri-armada-9.png";
+import armada10 from "../assets/images/galeri/galeri-armada-10.png";
+import armada11 from "../assets/images/galeri/galeri-armada-11.png";
+import armada12 from "../assets/images/galeri/galeri-armada-12.png";
+import truck1 from "../assets/images/galeri/truck-01.jpg";
+import truck2 from "../assets/images/galeri/truck-02.jpg";
+import truck3 from "../assets/images/galeri/truck-03.jpg";
+import truck4 from "../assets/images/galeri/truck-04.jpg";
+import truck5 from "../assets/images/galeri/truck-05.jpg";
+import truck6 from "../assets/images/galeri/truck-06.jpg";
+import truck7 from "../assets/images/galeri/truck-07.jpg";
+import truck8 from "../assets/images/galeri/truck-08.jpg";
 
 export const galleryData = [
-  {
-    id: 1,
-    image: galeri1,
-    caption: "Armada tangki air bersih siap kirim ke pelanggan",
-    category: "Armada",
-  },
-  {
-    id: 2,
-    image: galeri2,
-    caption: "Fasilitas penyaringan air tahap sedimentasi",
-    category: "Fasilitas",
-  },
-  {
-    id: 3,
-    image: galeri3,
-    caption: "Proses pengiriman air ke lokasi pelanggan",
-    category: "Pengiriman",
-  },
-  {
-    id: 4,
-    image: galeri4,
-    caption: "Tim operasional TIRTA KENCANA ABADI",
-    category: "Tim",
-  },
-  {
-    id: 5,
-    image: "https://picsum.photos/seed/galeri-fasilitas-2/800/600",
-    caption: "Laboratorium uji kualitas air",
-    category: "Fasilitas",
-  },
-  {
-    id: 6,
-    image: "https://picsum.photos/seed/galeri-armada-2/800/600",
-    caption: "Perawatan rutin armada tangki",
-    category: "Armada",
-  },
-  {
-    id: 7,
-    image: "https://picsum.photos/seed/galeri-pengiriman-2/800/600",
-    caption: "Pengisian air di lokasi klien industri",
-    category: "Pengiriman",
-  },
-  {
-    id: 8,
-    image: "https://picsum.photos/seed/galeri-kantor-1/800/600",
-    caption: "Kantor operasional TIRTA KENCANA ABADI",
-    category: "Fasilitas",
-  },
+  { id: 1, image: galeri1, caption: "Armada tangki air siap kirim ke pelanggan", category: "Armada" },
+  { id: 2, image: galeri2, caption: "Pengisian tangki air pada malam hari", category: "Armada" },
+  { id: 3, image: galeri3, caption: "Dua unit armada tangki siap beroperasi", category: "Armada" },
+  { id: 4, image: galeri4, caption: "Pengiriman air ke lokasi pelanggan", category: "Pengiriman" },
+  { id: 5, image: armada5, caption: "Armada tangki melintas menuju lokasi pengiriman", category: "Armada" },
+  { id: 6, image: armada6, caption: "Pengisian tangki air di malam hari", category: "Armada" },
+  { id: 7, image: armada7, caption: "Beberapa unit armada tangki di area operasional", category: "Armada" },
+  { id: 8, image: armada8, caption: "Armada tangki singgah di titik pengiriman", category: "Pengiriman" },
+  { id: 9, image: armada9, caption: "Unit armada tangki siap beroperasi", category: "Armada" },
+  { id: 10, image: armada10, caption: "Proses pengisian air di lokasi proyek", category: "Pengiriman" },
+  { id: 11, image: armada11, caption: "Tim operasional saat proses pengisian air", category: "Pengiriman" },
+  { id: 12, image: armada12, caption: "Armada tangki terparkir di area operasional", category: "Armada" },
+  { id: 13, image: truck1, caption: "Dokumentasi armada tangki TIRTA KENCANA ABADI", category: "Armada" },
+  { id: 14, image: truck2, caption: "Proses pengisian air bersih", category: "Pengiriman" },
+  { id: 15, image: truck3, caption: "Armada tangki siap melayani pelanggan", category: "Armada" },
+  { id: 16, image: truck4, caption: "Pengiriman air ke lokasi pelanggan", category: "Pengiriman" },
+  { id: 17, image: truck5, caption: "Unit armada tangki dalam kondisi prima", category: "Armada" },
+  { id: 18, image: truck6, caption: "Proses distribusi air bersih", category: "Pengiriman" },
+  { id: 19, image: truck7, caption: "Armada tangki siap beroperasi", category: "Armada" },
+  { id: 20, image: truck8, caption: "Dokumentasi kegiatan pengiriman air", category: "Pengiriman" },
 ];
